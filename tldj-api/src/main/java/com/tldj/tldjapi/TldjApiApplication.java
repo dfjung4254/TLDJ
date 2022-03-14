c2cc1060ecb80f0dd1a42bdc3b1538d6c1cefe41
dfjung4254 <rmsghk4254@naver.com>
2022-03-14 10:09:32 +0900

    initial api project

---
+++ tldj-api/src/main/java/com/tldj/tldjapi/TldjApiApplication.java
@@ -0,0 +1,13 @@
+package com.tldj.tldjapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TldjApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TldjApiApplication.class, args);
+  }
+
+}
---
+++ tldj-api/src/test/java/com/tldj/tldjapi/TldjApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tldj.tldjapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TldjApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
